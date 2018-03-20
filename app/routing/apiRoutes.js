98a3fea3306f78d79effa489b3e6e08b38bcd2bc
Kate Birmingham <kabi6686@colorado.edu>
2018-03-20 14:59:18 -0600

    fixed submit button functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,16 +11,51 @@ module.exports = function(app) {
 
     // Create New Characters - takes in JSON input
     app.post("/api/friends", function(req, res) {
+      console.log(req.body);
 
-      for (var i = 0; friendsData.length; i++) {
+      // object to update each new friend match
+      var friendMatch = {
+        name: "",
+        photo: "",
+        friendDifference: Infinity
+      };
 
-        console.log(req.body);
+      // take result of user response/parse with body-parser
+      var userData = req.body;
+      var userScores = userData.scores;
+      console.log("userScores", userScores);
 
-        friendsData.push(req.body);
+      // calculate difference between user score/database score
+      var scoreDifference;
+
+      // loop for friends in database
+      for (var i = 0; i < friendsData.length; i++) {
+
+        var friend = friendsData[i];
+        scoreDifference = 0;
+
+        //loop for scores in database
+        console.log("friend", friend);
+        for (var j = 0; j < friend.scores.length; j++) {
+          var friendScore = friend.scores[j];
+          var userScore = userScores[j];
+
+          // difference between scores
+          scoreDifference += Math.abs(parseInt(userScore) - parseInt(friendScore));
+        }
+
+        if (scoreDifference <= friendMatch.friendDifference) {
+          //set match to new friend
+          friendMatch.name = friend.name;
+          friendMatch.photo = friend.photo;
+          friendMatch.friendDifference = scoreDifference;
+        }
 
       }
 
-      return res.json(friendsData);
+      friendsData.push(userData)
+
+      return res.json(friendMatch);
     });
 
 } // END EXPORTS FUNCTION
